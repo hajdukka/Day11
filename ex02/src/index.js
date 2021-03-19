e4e7bd1e86c8974ff550ce350a7beec647781244
hajdukka <nerminhajdo@gmail.com>
2021-03-19 09:50:26 +0100

    ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,27 @@
+var temps = [
+  [31, 32, 19, 37],
+  [29, 27, 55, 36],
+  [17, 27, 42, 46],
+  [29, 52, 21, 64],
+  [91, 27, 31, 61],
+];
+function myArrayFunction(arr) {
+  var newTemps = [...arr];
+  var averageDayTemp = [];
+
+  for (var i = 0; i < 5; i++) {
+    var a = 0;
+
+    for (var j = 0; j < 4; j++) {
+      a += newTemps[i][j];
+    }
+
+    a = a / 4;
+    averageDayTemp.push(a);
+  }
+
+  return averageDayTemp;
+}
+
+console.log(myArrayFunction(temps));
+module.exports = myArrayFunction;
